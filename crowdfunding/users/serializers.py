e8aa6743247604b8c17ac70ecb61f9a251243be4
mkeerthiasm <mkeerthi.ayala@gmail.com>
2024-01-26 23:57:15 +0800

    created a new folder to store all API method screenshots and created a ER diagram

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -7,6 +7,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = '__all__'
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
+    
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
         return user    
