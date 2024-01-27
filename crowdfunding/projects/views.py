229e31525d6dfc07c7abcaaeaf1303afed30e9bd
mkeerthiasm <mkeerthi.ayala@gmail.com>
2024-01-28 01:16:07 +0800

    updated pledge details

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -73,3 +73,32 @@ class PledgeList(APIView):
             serializer.data,
             status=status.HTTP_400_BAD_REQUEST
          )         
+      
+class PledgeDetail(APIView):
+   permission_classes = [
+      permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,IsSupporterOrReadOnly
+   ]
+   try:
+     def get_object(self, pk):
+      pledge = Pledge.objects.get(pk=pk)
+      self.check_object_permissions(self.request, pledge)
+      return pledge
+   except Pledge.DoesNotExist:
+       raise Http404
+
+   def get(self, request, pk):
+      pledge = self.get_object(pk)
+      serializer = PledgeDetailSerializer(pledge)
+      return Response(serializer.data)
+   
+   def put (self, request,pk):
+      pledge = self.get_object(pk)
+      serializer = PledgeDetailSerializer(
+         instance=pledge,
+         data=request.data,
+         partial=True
+      )
+      if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data)
+
